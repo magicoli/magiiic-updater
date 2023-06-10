1b4e863558cfcb5aa6abf7b5de83824fb12780d5
Olivier van Helden <olivier@van-helden.net>
2023-06-10 08:36:16 -0400

    Initial wp-cli scaffold

---
+++ magiiic-updater.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Magiiic Updater
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Deliver updates for plugins provided my https://magiiic.com
+ * Author:          Magiiic
+ * Author URI:      https://magiiic.com
+ * Text Domain:     magiiic-updater
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Magiiic_Updater
+ */
+
+// Your code starts here.
